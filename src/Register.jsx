6a060bb4af1e9967886502329641e188f338da0e
najmulhouda <najmulhouda6@gmail.com>
2022-11-08 01:02:56 +0600

    style added

--- src/Register.jsx
+++ src/Register.jsx
@@ -15,12 +15,12 @@ export const Register = (props) => {
             <h2>Register</h2>
         <form className="register-form" onSubmit={handleSubmit}>
             <label htmlFor="name">Full name</label>
-            <input value={name} name="name" id="name" placeholder="full Name" /> <br /> <br />
+            <input value={name} name="name" id="name" placeholder="full Name" /> 
             <label htmlFor="email">email</label>
-            <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" /> <br /> <br />
+            <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" /> 
             <label htmlFor="password">password</label>
-            <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" /> <br /><br />
-            <button type="submit">Log In</button> <br /> <br />
+            <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" /> 
+            <button type="submit">Log In</button> 
         </form>
         <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button>
     </div>
